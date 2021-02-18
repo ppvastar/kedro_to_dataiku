03740ff21bd52da710a2e51fd45b4ee878e5add4
ppvastar <goodpustc@gmail.com>
2021-02-18 20:11:58 +0900

    v0.1

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from kedro_to_dataiku import __version__
 
 setup(name='kedro_to_dataiku',
-      version='0.0.1',
+      version=__version__,
       description='Deploy Kedro project to Dataiku',
       author='Peng Zhang',
       author_email='p.zhang@zoho.com',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-      license='LICENSE.txt',
+      install_requires=['pyspark','kedro==0.16.5','dataiku-api-client>=8.0.0'],
+      license='LICENSE.txt'
     )
